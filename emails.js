98dcfc766895ca4896120cff08004ec926a41b4f
Mitchell Rodio <Mitchellrodio@gmail.com>
2020-01-19 10:23:47 -0700

    email grabbed

---
+++ emails.js
@@ -0,0 +1,11 @@
+console.log("Im here!");
+
+document.getElementById("submit").addEventListener("click", function() {
+        var emails = {
+          email: document.getElementById('email').value,
+        };
+  
+       console.log(emails);
+        });
+
+        
